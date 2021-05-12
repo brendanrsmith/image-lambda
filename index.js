2c0416b21f62c644e11347e62d269923663ba536
Brendan Smith <brendan5320@gmail.com>
2021-05-12 10:17:56 -0700

    stopping point 4

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-
 const { S3 } = require ('@aws-sdk/client-s3');
 const client = new S3({ region: 'us-west-2', aws_access_key_id: 'AKIAQWD5F3LGKMR4QWWI', aws_secret_access_key: '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' })
 
 exports.handler = async (event, context, callback) => {
 
   let pics = [];
+  const bucket = event.Records[0].s3.bucket.name;
 
   console.log('EVENT DETAILS!', JSON.stringify(event, undefined, 2));
 
   try {
 
     // list all objects in brs-lab-17 bucket
-    const data = await client.listObjectsV2({ Bucket: 'brs-lab-17' });
+    const data = await client.listObjectsV2({ Bucket: bucket });
 
     // map those contents to pics array
     pics = data.Contents.map((element) => {
@@ -26,7 +26,7 @@ exports.handler = async (event, context, callback) => {
     console.log('pics!', pics);
 
     // TODO: get images.json file from bucket
-    const imagesJson = await client.getObject({ Bucket: 'arn:aws:s3:us-west-2:047507167948:accesspoint/imagejson', Key: 'images.json' });
+    const imagesJson = await client.getObject({ Bucket: bucket, Key: 'images.json' });
     console.log('imagesJSON', imagesJson);
 
     // TODO: re-write images.json file with new pics array 
