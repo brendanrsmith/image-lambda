feb9b154b60d8c120670da664825ae764d8e9228
Brendan Smith <brendan5320@gmail.com>
2021-05-12 08:36:30 -0700

    wednesday AM work

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { GetObjectCommand, S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 
-const client = new S3Client({ region: 'us-west-2'})
-const command = new ListObjectsV2Command({Bucket: 'brs-lab-17'});
-const getObject = new GetObjectCommand({Bucket: 'arn:aws:s3:us-west-2:047507167948:accesspoint/imagejson', Key: 'images.json'});
+const { S3 } = require ('@aws-sdk/client-s3');
+const client = new S3({ region: 'us-west-2', aws_access_key_id: 'AKIAQWD5F3LGKMR4QWWI', aws_secret_access_key: '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' })
+// const command = new ListObjectsV2Command({ Bucket: 'brs-lab-17' });
+// const getObject = new GetObjectCommand({ Bucket: 'arn:aws:s3:us-west-2:047507167948:accesspoint/imagejson', Key: 'images.json' });
 
 exports.handler = async (event, context, callback) => {
 
@@ -14,21 +14,21 @@ exports.handler = async (event, context, callback) => {
 
   try {
     // list all objects in brs-lab-17 bucket
-    const data = await client.send(command);
-    // console.log('DATA FROM S3', data.Contents);
-    
+    // const data = await client.send(command);
+    const data = await client.listObjectsV2({ Bucket: 'brs-lab-17' }); // try this new way
+
     // add those contents to pics array
     const contents = data.Contents;
-    contents.forEach(element => {
-      pics.push({
+    pics = contents.map((element) => {
+      return {
         name: element.Key,
         size: element.Size,
         lastModified: element.LastModified
-      })
-    });
+      }
+    })
     console.log('pics!', pics);
     // get images.json file from bucket
-    const imagesJson = await client.send(getObject);
+    const imagesJson = await client.getObject({ Bucket: 'arn:aws:s3:us-west-2:047507167948:accesspoint/imagejson', Key: 'images.json' }); // try this new way
     console.log('imagesJSON', imagesJson);
 
     // TODO: re-write images.json file with new pics array 
@@ -40,14 +40,3 @@ exports.handler = async (event, context, callback) => {
     return "there was an error connnecting to S3";
   }
 }
-
-// data: 
-  // Contents: [
-  //   {
-  //     Key: 'Screen Shot 2021-05-11 at 1.59.35 PM.png',
-  //     LastModified: 2021-05-11T21:11:34.000Z,
-  //     ETag: '"0c5a3f79e6722cec93a225cf77df9d91"',
-  //     Size: 321248,
-  //     StorageClass: 'STANDARD',
-  //     Owner: undefined
-  //   },
